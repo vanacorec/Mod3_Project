d065a409bcd64379b7adb53f4b495e8dd9b012da
Caroline Vanacore <vanacorec@gmail.com>
2019-04-04 11:58:18 -0400

    Add comments to plotting and modelling files
    Former-commit-id: d4c3eca1ce57650ba9c87eb0e0a2c64f8fa5509e

--- plot.py
+++ plot.py
@@ -9,22 +9,37 @@ def plot_confusion_matrix(cm, classes=['NonTroll','Troll'],
                           normalize=True,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
-    #Add Normalization Option
+    """ Prints out and plots a confusion matrix 
+
+    Parameters: 
+        cm (confusion matrix): output of sklearn.metrics.confusion_matrix function
+        classes (list) (optional): list of classes, default is NonTroll and Troll 
+        normalize (boolean) (optional): normalize labels if true
+        title (string): title of the plot, default "Confusion Matrix"
+        cmap (matplotlib colormap) (optional): default is plt.cm.Blues
+
+    Returns:
+        None
+    """
+    
+
+    #normalize the labels if normalize parameter is True
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
     else:
         print('Confusion matrix, without normalization')
 
+    #print the confusion matrix
     print(cm)
 
+    #format and display the confusion matrix
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
-
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
@@ -39,16 +54,36 @@ def plot_confusion_matrix(cm, classes=['NonTroll','Troll'],
 
 
 def get_auc_score(y_test, y_score):
+    """ Creates a receiver operating characteristic curve for y_test and y_score, and returns the area under the curve
+
+    Parameters:
+        y_test (series): class labels for test subset from train_test_split
+        y_score (np.array): array of probabiliites of prediction for one of the classes 
+
+    Return:
+        auc_score (int): area under the ROC curve
+    """
     fpr, tpr, thresholds = roc_curve(y_test,y_score)
     return auc(fpr,tpr)
 
 
 def plot_roc(y_test, y_score):
-    fpr, tpr, thresholds = roc_curve(y_test, y_score)
-    
-    #sns.set_style("darkgrid", {"axes.facecolor": ".9"})
+    """ Plots a receiver operating characteristic curve (true positive rate against false positive rate at various thresholds)
+    for y_test and y_scores
 
+    Parameters:
+        y_test (series): test class labels from train_test_split
+        y_score (np.array): array of probabiliites of prediction for one of the classes
+    """
+
+    # use y_test and y_score to get false positive rates and true positive rates for different thresholds 
+    fpr, tpr, thresholds = roc_curve(y_test, y_score)
+
+
+    #print the area under the ROC curve based on false positive rates and true positive rates
     print('AUC: {}'.format(auc(fpr, tpr)))
+
+    # format and display the ROC curve
     plt.figure(figsize=(10,8))
     lw = 2
     plt.plot(fpr, tpr, color='darkorange',
--- russian_models.py
+++ russian_models.py
@@ -1,5 +1,4 @@
 import numpy as np
-from sklearn.metrics import confusion_matrix
 import plot
 from sklearn.linear_model import SGDClassifier,LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
@@ -8,6 +7,20 @@ from sklearn.metrics import confusion_matrix, classification_report, roc_curve, 
 
 
 def strut_models(classifiers,data_split,output=False):
+    """ Takes in a list of models and training and testing data, 
+    fits each model on the training data, and prints out the classification report and confusion matrix
+
+    Parameters:
+        classifiers (list): list of model objects
+        data_split (list): list of outputs from train_test_split [X_train, X_test, y_train, y_test]
+
+    Returns:
+        if the output parameter is True:
+            model (model): fitted input model if output is true
+            prob (np.array): array of two columns, each col is probabilities of predictions for a class
+        else:
+            None
+    """
     for classifier in classifiers:
         model = classifier.fit(data_split[0],data_split[2])
         prediction = classifier.predict(data_split[1])
@@ -20,12 +33,45 @@ def strut_models(classifiers,data_split,output=False):
         return model, prob
 
 def predict(function,nt_pred,tfidf_pred,y_test,weight=1):
-    tfidf_votes = function(nt_pred*weight,tfidf_pred)
-    
+    """ Takes in arrays probabailites of class predictions for 2 different models, combines them based on 
+    the input function, and prints out the classification report and plots the confusion matrix
+
+    Parameters:
+        function (function): function that determines how probability predictions will be combined
+        nt_pred (np.array): probabilities of prediction for each class from one model (non-text)
+        tfidf_pred (np.array):  probabilities of prediction for each class from another model (text)
+        y_test (series): class label values for test data
+        weight (int) (optional): number by which to multiply nt_pred values to weight them more or less
+    Returns:
+        None
+    """
+
+    votes = function(nt_pred*weight,tfidf_pred)
     print('Combined Votes:')
-    plot.plot_confusion_matrix(confusion_matrix(y_test,tfidf_votes))
+    print(classification_report(y_test, votes))
+    plot.plot_confusion_matrix(confusion_matrix(y_test,votes))
+
+
+"""
+For the following functions (pred_* ):
+    Parameters: 
+        non_text (np.array): 2 column array, first and second columns are predicted probabilities of the corresponding row being a nontroll 
+        and troll, respectively. Array is output of model.predict_proba(y_test), where model is fit with non text data
+        text (np.array): 2 column array, first and second columns are predicted probabilities of the corresponding row being a nontroll 
+        and troll, respectively. Array is output of model.predict_proba(y_test), where model is fit with text data
+
+
+    Returns:
+        votes: 1 column array with final predictions of nontroll (0) or troll (1) for each row
+"""
+
     
 def pred_geometric(non_text,text):
+    """ 
+    Combines the probabilities from two models by taking the mean of non-troll probabilities for each row 
+    and mean of troll probabilities for each row, and returning an array with 0 if non-troll mean is higher
+    or 1 if lower
+    """
     votes = []
     for nt_pred,t_pred in zip(non_text,text):
         vote_null = (nt_pred[0]*t_pred[0])**.5
@@ -34,6 +80,11 @@ def pred_geometric(non_text,text):
     return votes 
 
 def pred_harmonic(non_text,text):
+    """ 
+    Combines the probabilities from two models by taking the harmonic mean of non-troll probabilities for each row 
+    and harmonic mean of troll probabilities for each row, and returning an array with 0 if non-troll mean is higher
+    or 1 if lower
+    """
     votes = []
     for nt_pred,t_pred in zip(non_text,text):
         vote_null = 2*nt_pred[0]*t_pred[0]/(nt_pred[0]+t_pred[0])
@@ -42,6 +93,10 @@ def pred_harmonic(non_text,text):
     return votes    
 
 def pred_confidence(non_text,text):
+    """ 
+    Combines the probabilities from two models by determinining the differences in probabilities  
+    between the two models for each class, and setting the final vote of whichever model is more confident
+    """
     votes = []
     for nt_pred,t_pred in zip(non_text,text):
         vote_text = t_pred[1]-t_pred[0]
@@ -50,13 +105,22 @@ def pred_confidence(non_text,text):
     return votes  
 
 def pred_max(non_text,text):
+    """ 
+    Combines predictionprobabilities from two models by adding non-troll probabilities for each row together 
+    and the troll probabalities for each row and using whichever sum is greater as the prediction
+    """
+
     votes = []
     for nt_pred,t_pred in zip(non_text,text):
         vote = nt_pred+t_pred
         votes.append(0 if vote[0]>vote[1] else 1)
     return votes    
 
-def decision_func(non_text,text,weight=1):
+def decision_func(non_text,text):
+    """
+    Combines predictions of two models by summing predictions for each class and outputs total predictions
+    for one of the classes, to be used to as y_cores in the ROC curve 
+    """
     confidences = []
     for nt_pred,t_pred in zip(non_text,text):
         vote = nt_pred+t_pred
